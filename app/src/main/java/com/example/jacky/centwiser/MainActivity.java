6e441740b2437b7030b01ddd1cb86ff2ee936268
kttkjl <jinglun.li@alumni.ubc.ca>
2018-10-17 22:03:30 -0700

    rudamentary activities added

--- app/src/main/java/com/example/jacky/centwiser/MainActivity.java
+++ app/src/main/java/com/example/jacky/centwiser/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.jacky.centwiser;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView map_categories = findViewById(R.id.listview_map_categories);
+
+        // We grab a constant array for category
+        final String [] const_map_categories = getResources().getStringArray(R.array.const_map_categories);
+
+        map_categories.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            // Adapter view just KNOWS which thing is in the category
+            System.out.println("Clicked position: " + i + ", row_id: " + l);
+            Intent intent = new Intent(MainActivity.this, GoogleMapActivity.class);
+            intent.putExtra("map_category", const_map_categories[i]);
+            startActivity(intent);
+            }
+        });
+
     }
 }
