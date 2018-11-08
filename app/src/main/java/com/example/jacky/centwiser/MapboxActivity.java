319d4a453e84cd3872a877b69e6f428f9ef5ded8
Keishi Asai <isheik808@gmail.com>
2018-11-07 21:02:26 -0800

    Implement Mapbox and async download for DropIn

--- app/src/main/java/com/example/jacky/centwiser/MapboxActivity.java
+++ app/src/main/java/com/example/jacky/centwiser/MapboxActivity.java
@@ -1,7 +1,12 @@
 package com.example.jacky.centwiser;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.geometry.LatLng;
@@ -9,8 +14,18 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MapboxActivity extends AppCompatActivity {
     private MapView mapView;
+
+    private String TAG = MainActivity.class.getSimpleName();
+    private ProgressDialog pDialog;
+
+    // URL to get contacts JSON
+    private static String SERVICE_URL = "http://opendata.newwestcity.ca/downloads/drop-in-centres/DROPIN_CENTRE.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +44,8 @@ public class MapboxActivity extends AppCompatActivity {
                         .build());
             }
         });
+
+        new GetDropInCentres().execute();
     }
 
     @Override
@@ -70,5 +87,69 @@ public class MapboxActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
+    }
+
+    /**
+     * Async task class to get json by making HTTP call
+     */
+    private class GetDropInCentres extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(MapboxActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(SERVICE_URL);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject dropInCentres = new JSONObject(jsonStr);
+                    Log.i("fromjson", dropInCentres.toString());
+                } catch (final JSONException e) {
+                    Log.i("dropIn", "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+        }
     }
 }
