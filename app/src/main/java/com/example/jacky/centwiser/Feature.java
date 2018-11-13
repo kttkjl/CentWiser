a2aa3934d06f7db1f61a96e00e7c6db00889ca40
Keishi Asai <isheik808@gmail.com>
2018-11-13 12:34:21 -0800

    Add Feature class

---
+++ app/src/main/java/com/example/jacky/centwiser/Feature.java
@@ -0,0 +1,102 @@
+package com.example.jacky.centwiser;
+
+public class Feature {
+    private String type;
+
+    private String Name;
+
+    private String Description;
+
+    private String Category;
+
+    private String Hours;
+
+    private String Location;
+
+    private String PC;
+
+    private String Phone;
+
+    private String Email;
+
+    private String Website;
+
+    private String X;
+
+    private String Y;
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+    public void setName(String Name){
+        this.Name = Name;
+    }
+    public String getName(){
+        return this.Name;
+    }
+    public void setDescription(String Description){
+        this.Description = Description;
+    }
+    public String getDescription(){
+        return this.Description;
+    }
+    public void setCategory(String Category){
+        this.Category = Category;
+    }
+    public String getCategory(){
+        return this.Category;
+    }
+    public void setHours(String Hours){
+        this.Hours = Hours;
+    }
+    public String getHours(){
+        return this.Hours;
+    }
+    public void setLocation(String Location){
+        this.Location = Location;
+    }
+    public String getLocation(){
+        return this.Location;
+    }
+    public void setPC(String PC){
+        this.PC = PC;
+    }
+    public String getPC(){
+        return this.PC;
+    }
+    public void setPhone(String Phone){
+        this.Phone = Phone;
+    }
+    public String getPhone(){
+        return this.Phone;
+    }
+    public void setEmail(String Email){
+        this.Email = Email;
+    }
+    public String getEmail(){
+        return this.Email;
+    }
+    public void setWebsite(String Website){
+        this.Website = Website;
+    }
+    public String getWebsite(){
+        return this.Website;
+    }
+    public void setX(String X){
+        this.X = X;
+    }
+    public String getX(){
+        return this.X;
+    }
+    public void setY(String Y){
+        this.Y = Y;
+    }
+    public String getY(){
+        return this.Y;
+    }
+}
